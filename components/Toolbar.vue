80b68101ffc9ac9ac4845b142cebc2d676c201b1
Brian Summers <briansummers14@gmail.com>
2017-12-12 02:07:35 -0700

    Nuxt Files

---
+++ components/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <nav class="teal lighten-2">
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="collapsible.html">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+</template>
+
+<style>
+.nav-wrapper {
+    padding: 0 12px;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'static-template',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
